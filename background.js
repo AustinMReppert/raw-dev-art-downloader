e6118c526dbe2b3b2f30522b4f5417859364ff12
Austin M. Reppert <austinmreppert@gmail.com>
2020-01-29 01:19:17 -0500

    remove setTimeout

--- background.js
+++ background.js
@@ -1,13 +1,12 @@
-chrome.runtime.onMessage.addListener( (request, sender, sendResponse) => {
-    if(request === null || request.length === 0) return;
-    var name = request[0].substring(34);
+const downloadImage = (images, imageNum) => {
+    var name = images[0].substring(34);
     name = name.substring(0, name.indexOf("/"));
-    console.log(name);
-    for(i = 0; i < request.length; i++) {
-     //   setTimeout(() => {
-        var filename = name + "/" + i + ".jpg";
-        console.log(filename);
-            chrome.downloads.download({url: request[i]});
-       // }, 1000);
-    }
+    var filename = name + "/" + imageNum + ".jpg";
+    chrome.downloads.download({ url: images[imageNum], filename: filename });
+}
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request === null || request.length === 0) return;
+    for (i = 0; i < request.length; i++)
+        downloadImage.bind(request, i);
 });
